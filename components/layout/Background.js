b7a5b6c81e968f7c56b066a490f4fa689ed8d99d
cheba91 <blaz.cebasek@gmail.com>
2022-08-03 18:01:35 +0200

    small fix

--- components/layout/Background.js
+++ components/layout/Background.js
@@ -18,7 +18,7 @@ export default function Background({ color, children }) {
             background: 'rgba(0,0,0,1) center center absolute',
          }}
       >
-         <css-doodle color="transparent" key={Math.random()}>
+         <css-doodle style={{ color: 'transparent' }} key={Math.random()}>
             {`
                 :doodle {
                 @grid: 1x3 / 98vmax;
