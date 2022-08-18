726a1f52b2da61c577332166fafb3e9849cc5a30
cheba91 <blaz.cebasek@gmail.com>
2022-08-18 20:14:35 +0200

    box shadow fixes

--- components/About.js
+++ components/About.js
@@ -3,6 +3,7 @@ import { useEffect, useState } from 'react';
 import myImage from '../public/myImage.png';
 import AnchorId from './ui/AnchorId';
 import NeonHeading from './ui/NeonHeading';
+import NeonShadow from './ui/NeonShadow';
 
 export default function About() {
    const imgSize = 190;
@@ -37,154 +38,157 @@ export default function About() {
    return (
       <>
          <AnchorId id={'about'} />
-         <Grid
-            mb={10}
-            container
-            rows={2}
-            sx={{
-               position: 'relative',
-               zIndex: '0',
-               borderRadius: `${borderRadius}px`,
-               overflow: 'hidden',
-               padding: '2rem',
-               '&::before': {
-                  overdlow: 'hidden',
-                  content: "''",
-                  position: 'absolute',
-                  zIndex: '-1',
-                  left: '-150%',
-                  top: '-150%',
-                  width: '400%',
-                  height: '400%',
-                  backgroundRepeat: 'no-repeat',
-                  backgroundSize: '50% 50%, 50% 50%',
-                  backgroundPosition: '0 0, 100% 0, 100% 100%, 0 100%',
-                  backgroundImage: `linear-gradient(to  left, ${mainClr} 1%, rgba(0,0,0, 0) 5%),
-                  linear-gradient(transparent, transparent),
-                     linear-gradient(to right, ${mainClr} 1%, rgba(0,0,0,0) 5% ),
-                     linear-gradient(transparent, transparent)`,
-                  animation: 'rotate 8s linear infinite',
-               },
-               '&::after': {
-                  content: "''",
-                  position: 'absolute',
-                  overflow: 'hidden',
-                  zIndex: '-1',
-                  left: '3px',
-                  top: '3px',
-                  width: 'calc(100% - 6px)',
-                  height: 'calc(100% - 6px)',
-                  background: cardBg,
+         <NeonShadow radius={`${borderRadius}px`}>
+            <Grid
+               mb={10}
+               container
+               rows={2}
+               sx={{
+                  position: 'relative',
+                  zIndex: '0',
                   borderRadius: `${borderRadius}px`,
-               },
-               '@keyframes rotate': {
-                  '100%': { transform: 'rotate(1turn)' },
-               },
-               borderRadius: `${borderRadius}px`,
-               padding: '1rem 1rem 1.5rem 1rem',
-            }}
-         >
-            <Grid width="100%">
-               <NeonHeading text={'About me'} />
-            </Grid>
-            {/* Text */}
-            <Grid item xs={12} sm={7} padding=" 0 1.5rem 1.5rem 1.5rem">
-               <Typography mb={2}>
-                  {`Hey, I'm good at reading docs ;)`}
-               </Typography>
-               <Typography>
-                  {`I'm a self-taught web developer from Slovenia and I've been in web development for 4+ years. 
+                  overflow: 'hidden',
+                  padding: '2rem',
+                  background: cardBg,
+                  // '&::before': {
+                  //    overdlow: 'hidden',
+                  //    content: "''",
+                  //    position: 'absolute',
+                  //    zIndex: '-1',
+                  //    left: '-150%',
+                  //    top: '-150%',
+                  //    width: '400%',
+                  //    height: '400%',
+                  //    backgroundRepeat: 'no-repeat',
+                  //    backgroundSize: '50% 50%, 50% 50%',
+                  //    backgroundPosition: '0 0, 100% 0, 100% 100%, 0 100%',
+                  //    backgroundImage: `linear-gradient(to  left, ${mainClr} 1%, rgba(0,0,0, 0) 5%),
+                  //    linear-gradient(transparent, transparent),
+                  //       linear-gradient(to right, ${mainClr} 1%, rgba(0,0,0,0) 5% ),
+                  //       linear-gradient(transparent, transparent)`,
+                  //    animation: 'rotate 8s linear infinite',
+                  // },
+                  // '&::after': {
+                  //    content: "''",
+                  //    position: 'absolute',
+                  //    overflow: 'hidden',
+                  //    zIndex: '-1',
+                  //    left: '3px',
+                  //    top: '3px',
+                  //    width: 'calc(100% - 6px)',
+                  //    height: 'calc(100% - 6px)',
+                  //    background: cardBg,
+                  //    borderRadius: `${borderRadius}px`,
+                  // },
+                  // '@keyframes rotate': {
+                  //    '100%': { transform: 'rotate(1turn)' },
+                  // },
+                  // borderRadius: `${borderRadius}px`,
+                  // padding: '1rem 1rem 1.5rem 1rem',
+               }}
+            >
+               <Grid width="100%">
+                  <NeonHeading text={'About me'} />
+               </Grid>
+               {/* Text */}
+               <Grid item xs={12} sm={7} padding=" 0 1.5rem 1.5rem 1.5rem">
+                  <Typography mb={2}>
+                     {`Hey, I'm good at reading docs ;)`}
+                  </Typography>
+                  <Typography>
+                     {`I'm a self-taught web developer from Slovenia and I've been in web development for 4+ years. 
                   My journey began with WordPress, but I soon shifted to the Javascript ecosystem and my current favorite framework Next.js.`}
-               </Typography>
-               <Typography mt={2}>
-                  {/* {`I love learning new things and working on creative projects.`} */}
-                  {`I enjoy working on creative projects and learning about new things. My code is clean and elegant, and I always prioritize security and performance.`}
-               </Typography>
-            </Grid>
-            {/* Image Section */}
-            <Grid item xs={12} sm={5} textAlign="center">
-               <Box
-                  sx={{
-                     width: '100%',
-                     height: '100%',
-                     position: 'relative',
-                     display: 'flex',
-                     justifyContent: 'center',
-                     alignItems: 'center',
-                     paddingTop: { xs: '2rem', sm: '0' },
-                     paddingLeft: { xs: '0', sm: '1rem' },
-                  }}
-               >
-                  {/* Pic wrap */}
+                  </Typography>
+                  <Typography mt={2}>
+                     {/* {`I love learning new things and working on creative projects.`} */}
+                     {`I enjoy working on creative projects and learning about new things. My code is clean and elegant, and I always prioritize security and performance.`}
+                  </Typography>
+               </Grid>
+               {/* Image Section */}
+               <Grid item xs={12} sm={5} textAlign="center">
                   <Box
                      sx={{
-                        height: imgSize,
-                        width: imgSize,
-                        // margin: '0 auto',
+                        width: '100%',
+                        height: '100%',
+                        position: 'relative',
+                        display: 'flex',
+                        justifyContent: 'center',
+                        alignItems: 'center',
+                        paddingTop: { xs: '2rem', sm: '0' },
+                        paddingLeft: { xs: '0', sm: '1rem' },
                      }}
                   >
-                     {/* Profile Pic */}
+                     {/* Pic wrap */}
                      <Box
-                        onMouseEnter={(e) => handleLaser(e, true)}
-                        onMouseLeave={(e) => handleLaser(e, false)}
-                        onClick={(e) => handleLaser(e, !laserVisible)}
-                        id="profilePic"
-                        component="img"
-                        alt="My image"
-                        src={myImage.src}
                         sx={{
-                           borderRadius: '50%',
-                           boxShadow: `3px 2px 0px 1px ${mainClr}`,
                            height: imgSize,
                            width: imgSize,
-                           maxWidth: '100%',
-                           maxHeight: '100%',
+                           // margin: '0 auto',
                         }}
-                     />
-                     {laserVisible && laserX && laserY && (
+                     >
+                        {/* Profile Pic */}
                         <Box
-                           id="laser"
+                           onMouseEnter={(e) => handleLaser(e, true)}
+                           onMouseLeave={(e) => handleLaser(e, false)}
+                           onClick={(e) => handleLaser(e, !laserVisible)}
+                           id="profilePic"
+                           component="img"
+                           alt="My image"
+                           src={myImage.src}
                            sx={{
-                              position: 'fixed',
-                              top: laserY,
-                              left: laserX,
-                              width: '28px',
-                              height: '100vh',
-                              background: 'none',
-
-                              borderLeft: `4px solid ${mainClr}`,
-                              borderRight: `4px solid ${mainClr}`,
-                              boxShadow: `inset 0px -20px 20px 1px ${mainClr}, 0px 10px 30px 1px ${mainClr}`,
-
-                              transformOrigin: 'top right',
-                              transform: 'rotate(40deg) skewY(-40deg)',
-                              '&::before': {
-                                 content: '""',
-                                 position: 'absolute',
-                                 width: 0,
-                                 height: 0,
-                                 boxShadow: `0px 2px 14px 6px #fff, 2px 3px 15px 15px ${mainClr}`,
-                                 borderRadius: '50%',
-                                 left: '-2px',
-                                 top: '2px',
-                              },
-                              '&::after': {
-                                 content: '""',
-                                 position: 'absolute',
-                                 width: 0,
-                                 height: 0,
-                                 boxShadow: `0px 2px 14px 6px #fff, 2px 3px 15px 15px ${mainClr}`,
-                                 borderRadius: '50%',
-                                 left: '20px',
-                                 top: '2px',
-                              },
+                              borderRadius: '50%',
+                              boxShadow: `3px 2px 0px 1px ${mainClr}`,
+                              height: imgSize,
+                              width: imgSize,
+                              maxWidth: '100%',
+                              maxHeight: '100%',
                            }}
                         />
-                     )}
+                        {laserVisible && laserX && laserY && (
+                           <Box
+                              id="laser"
+                              sx={{
+                                 position: 'fixed',
+                                 top: laserY,
+                                 left: laserX,
+                                 width: '28px',
+                                 height: '100vh',
+                                 background: 'none',
+
+                                 borderLeft: `4px solid ${mainClr}`,
+                                 borderRight: `4px solid ${mainClr}`,
+                                 boxShadow: `inset 0px -20px 20px 1px ${mainClr}, 0px 10px 30px 1px ${mainClr}`,
+
+                                 transformOrigin: 'top right',
+                                 transform: 'rotate(40deg) skewY(-40deg)',
+                                 '&::before': {
+                                    content: '""',
+                                    position: 'absolute',
+                                    width: 0,
+                                    height: 0,
+                                    boxShadow: `0px 2px 14px 6px #fff, 2px 3px 15px 15px ${mainClr}`,
+                                    borderRadius: '50%',
+                                    left: '-2px',
+                                    top: '2px',
+                                 },
+                                 '&::after': {
+                                    content: '""',
+                                    position: 'absolute',
+                                    width: 0,
+                                    height: 0,
+                                    boxShadow: `0px 2px 14px 6px #fff, 2px 3px 15px 15px ${mainClr}`,
+                                    borderRadius: '50%',
+                                    left: '20px',
+                                    top: '2px',
+                                 },
+                              }}
+                           />
+                        )}
+                     </Box>
                   </Box>
-               </Box>
+               </Grid>
             </Grid>
-         </Grid>
+         </NeonShadow>
       </>
    );
 }
