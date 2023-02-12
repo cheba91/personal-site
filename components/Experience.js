25aa8f1a3e30ea5e4fe78a4dd3bc05e5ce067ad4
cheba91 <blaz.cebasek@gmail.com>
2023-02-12 10:16:47 +0100

    mb 10

--- components/Experience.js
+++ components/Experience.js
@@ -6,55 +6,55 @@ import NeonShadow from './ui/NeonShadow';
 import AllTech from './AllTech';
 import OutsetShadow from './ui/OutsetShadow';
 export default function Experience() {
-   const {
-      palette: {
-         dark: { cardBg },
-      },
-      shape: { borderRadius },
-   } = useTheme();
+  const {
+    palette: {
+      dark: { cardBg },
+    },
+    shape: { borderRadius },
+  } = useTheme();
 
-   return (
-      <>
-         <AnchorId id={'experience'} />
-         <NeonShadow radius={`${borderRadius}px`}>
-            {/* Experience */}
-            <Box
-               mb={5}
-               sx={{
-                  borderRadius: `${borderRadius}px`,
-                  background: cardBg,
-                  padding: { xs: '1rem', sm: '2rem' },
-                  position: 'relative',
-                  width: '100%',
-                  height: '100%',
-                  overflow: 'hidden',
-               }}
-            >
-               <NeonHeading text={'Experience'} />
-               {/* Short desc */}
-               <OutsetShadow
-                  customStyles={{
-                     marginBottom: '4rem',
-                  }}
-               >
-                  <Typography
-                     sx={{
-                        padding: '2rem 1.5rem',
-                        textAlign: 'center',
-                        lineHeight: '1.6rem',
-                        fontSize: { xs: '0.95rem', sm: '1rem' },
-                     }}
-                  >{`I've been in web development for the past 4+ years. I began with WordPress, 
+  return (
+    <>
+      <AnchorId id={'experience'} />
+      <NeonShadow radius={`${borderRadius}px`}>
+        {/* Experience */}
+        <Box
+          mb={10}
+          sx={{
+            borderRadius: `${borderRadius}px`,
+            background: cardBg,
+            padding: { xs: '1rem', sm: '2rem' },
+            position: 'relative',
+            width: '100%',
+            height: '100%',
+            overflow: 'hidden',
+          }}
+        >
+          <NeonHeading text={'Experience'} />
+          {/* Short desc */}
+          <OutsetShadow
+            customStyles={{
+              marginBottom: '4rem',
+            }}
+          >
+            <Typography
+              sx={{
+                padding: '2rem 1.5rem',
+                textAlign: 'center',
+                lineHeight: '1.6rem',
+                fontSize: { xs: '0.95rem', sm: '1rem' },
+              }}
+            >{`I've been in web development for the past 4+ years. I began with WordPress, 
                   but lately, the MERN stack and Next.js have gotten most of my attention. I have 
                   primarily worked as a freelancer for a couple of web development studios, 
                   either by starting from scratch or optimizing, fixing or upgrading already 
                   existing projects. Please note that I won't be linking to any of the projects I worked on for them.`}</Typography>
-               </OutsetShadow>
-               <AllTech />
-               {/* Projects */}
-               <AllProjects />
-            </Box>
-         </NeonShadow>
-      </>
-   );
+          </OutsetShadow>
+          <AllTech />
+          {/* Projects */}
+          <AllProjects />
+        </Box>
+      </NeonShadow>
+    </>
+  );
 }
